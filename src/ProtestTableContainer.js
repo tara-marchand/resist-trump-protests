6e64beb137b2c46f6f039009002b842790172bb8
Tara Marchand <tara@mac.com>
2017-02-16 08:31:44 -0800

    Work in progress

--- src/ProtestTableContainer.js
+++ src/ProtestTableContainer.js
@@ -1,6 +1,69 @@
 import { orderBy } from 'lodash';
+import maps from '@google/maps'
+import moment from 'moment-timezone';
+import promise from 'promise'
 import React, { Component } from 'react';
 import ProtestTable from './ProtestTable'
+
+
+
+        // const self = this;
+
+        // window.gapi.client.init({
+        //     'apiKey': process.env['REACT_APP_API_KEY'],
+        //     'discoveryDocs': ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
+        // }).then(function() {
+        //     return window.gapi.client.request({
+        //         'path': 'https://sheets.googleapis.com/v4/spreadsheets/1BHLcMeNwk779OHpYjvMRqDQKXiJAqsmhkvrYQzU8CtE/values/A3:F'
+        //     })
+        // }).then(function(response) {
+        //     self.load(response);
+        // }, function(reason) {
+        //     console.log('Error: ' + reason.details);
+        // });
+
+const loadClient = (callback) => window.gapi.load('client', callback);
+// , () => {
+//     let protests = getProtests().then(processProtests());
+//     this.setState({protests});
+// });
+
+const initClient = () => window.gapi.client.init({
+    'apiKey': process.env['REACT_APP_API_KEY'],
+    'discoveryDocs': ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
+})
+
+const getSheet = () => window.gapi.client.request({
+    'path': 'https://sheets.googleapis.com/v4/spreadsheets/1BHLcMeNwk779OHpYjvMRqDQKXiJAqsmhkvrYQzU8CtE/values/A3:F'
+})
+
+const getProtests = (response) => {
+    let protests = response.values;
+    this.setState({protests});
+};
+
+const processProtests = (protests) => {
+    protests = protests.map((protest, i) => {
+        let row = i + 2;
+
+        let date = protest[0] || '';
+        let dateMoment = null;
+        let dateString = '';
+        if (date) {
+            dateMoment = moment(date, 'ddd, MMM D');
+            dateString = dateMoment.format('ddd MMM D YYYY')
+        }
+
+        let city = protest[1] || '';
+        let state = protest[2] || '';
+        let time = protest[3] || '';
+        let location = protest[4] || '';
+        let url = protest[5] || '';
+
+        return { row, dateString, city, state, time, location, url }
+    });
+    return orderBy(protests, ['date']);
+}
 
 class ProtestTableContainer extends Component {
     constructor() {
@@ -8,60 +71,9 @@ class ProtestTableContainer extends Component {
         this.state = { protests: [] }
     }
 
-    getData() {
-        const self = this;
-        window.gapi.client.init({
-            'apiKey': process.env['REACT_APP_API_KEY'],
-            'discoveryDocs': ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
-        }).then(function() {
-            return window.gapi.client.request({
-                'path': 'https://sheets.googleapis.com/v4/spreadsheets/1BHLcMeNwk779OHpYjvMRqDQKXiJAqsmhkvrYQzU8CtE/values/A3:F'
-            })
-        }).then(function(response) {
-            self.load(response);
-        }, function(reason) {
-            console.log('Error: ' + reason.details);
-        });
-    }
-
     componentDidMount() {
-        window.gapi.load('client', () => {
-            this.getData(true, this.load);
-        });
-    }
-
-    load(response) {
-        let protests = response.result.values || [];
-        protests = protests.map((protest, i) => {
-            let row = i + 2;
-
-            let date = protest[0] || '';
-            let dateString = '';
-            if (date) {
-                let dateArray = date.split(' ');
-                let year = dateArray.length > 3 ? parseInt(dateArray[3], 10) : 2017;
-                date = new Date(date + ' ' + year + ' ' + protest[3])
-                dateString = date.toDateString();
-            }
-
-            let city = protest[1] || '';
-            let state = protest[2] || '';
-            let time = protest[3] || '';
-            let location = protest[4] || '';
-            let url = protest[5] || '';
-
-            return {
-                row,
-                dateString,
-                city,
-                state,
-                time,
-                location,
-                url
-            }
-        });
-        protests = orderBy(protests, ['date'], ['time']);
-        this.setState({protests});
+        loadClient().then(initClient().then(getSheet));
+        // .then(initClient().then(getSheet().then(getProtests())));   
     }
 
     render() {
